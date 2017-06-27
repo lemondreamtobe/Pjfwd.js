7bca5ee99400d79c6f5b3d45f293c5bd2eb72f83
lemondreamtobe <897418436@qq.com>
2017-06-27 16:37:37 +0800

    3 chapter

---
+++ 3.chapter/js/3cp.js
@@ -0,0 +1,77 @@
+//ES5严格模式，'use strict',目地对ES3中不确定行为加以处理
+//支持严格模式浏览器：IE10+， fox4+, chrome, opera12+
+
+//具有可读性
+var a = 1,
+	b ='first',
+	c = ture;
+var ECMAScriptDataType = ['Undefined', 'Null', 'Boolean', 'Number', 'String'].concat(['Object']); //5种基本数据类型加一种复杂数据类型
+
+//测基本数据类型 typeof：操作符
+
+//Undefined派生自Null, undefined == null--true
+
+//Booean():转型函数，可对任何数据使用
+//假值： false, "", null, undefined, 0&NaN，记住6个假值其他类型值转换为true
+
+//Number:IEEE754格式，严格模式8进制无效
+//数值转换：Number(), parseInt(), parseFloat()
+/*
+ * Number():在++，--， +，位操作(~,|,&，^，<<,>>,>>>)对非数值应用会进行该规则的转换,
+ * 			Boolean		: true - 1 ; false - 0;
+ * 			Number      : number;
+ * 			Null        : null - 0;
+ * 			Undefined   : undefined - NaN;
+ * 			String      : 只有数字'123' - 123
+ * 						：只有数字和浮点'1.23' - 1.23
+ * 						: 有效16进制'0xa' - 10
+ * 						: "" - 0
+ * 						: 以上五条以外 's5' - NaN  '5s' - NaN
+ * 			Object      : valueOf() - if(NaN) - toString()，实际如果改写valueOf返回NaN,那么Number(object)就是NaN,不经过toString
+ * 
+ * 以下两种方法专用于转换字符串
+ * parseInt():
+ * 			"" - NaN ,与Number的不同之处
+ * 			ES3存在8进制，ES5不解析8进制
+ * 			不识别'.'
+ * 			提供第二个参数
+ * 			parseInt('0xaf', 16) - 175;
+ * 			parseInt('af') - NaN;
+ * 			parseInt('10', 2) - 2;
+ * 			parseInt('10', 8) - 8;
+ * 			parseInt('10', 10) - 10;
+ * 			parseInt('10', 16) - 16;
+ * parseFloat():
+ * 			"" - NaN
+ * 			只解析10进制，始终忽略前导0
+ * 			识别第一个'.',不识别第二个
+ * 			没有第二个参数
+ * 			parseFloat('1234b') - 1234
+ * 			parseFloat('0xa') - 0
+ * 			parseFloat('12.3') - 12.3
+ * 			parseFloat('12e3') - 12000， 可以科学计数法，parseInt不行
+ */
+//String转换。String(undefined / null)， 其他用toString(),有一个参数
+/*
+ * toString():
+ * 			var num = 10;
+ * 			num.toString() - '10'
+ * 			num.toString(2) - '1010'
+ * 			num.toString(8) - '12'
+ * 			num.toString(16) - 'a'
+ */
+/*
+ * Object类型是所有对像实例的基础，即所有对象的原型最终指向
+ * 			constructor:
+ * 			hasOwnProperty(prop):
+ * 			isPrototypeOf(object):
+ * 			propertyIsEnumerable(prop):
+ * 			toLocaleString():
+ * 			toString():
+ * 			valueOf():
+ */
+/*
+ * !:Booean()转换后取反
+ * &&:假值优先
+ * ||:真值优先
+ */
