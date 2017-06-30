41cf4d431231c68196320738c9c5ce2764f35c19
lemondreamtobe <897418436@qq.com>
2017-06-30 11:02:11 +0800

    add 作用域安全

--- 22.chapter/js/22cp.js
+++ 22.chapter/js/22cp.js
@@ -1,3 +1,102 @@
+//作用域安全
+function Person(name, age, job) {
+	
+	if (this instanceof Person) {
+		this.name = name;
+		this.job = job;
+		this.age = age;
+	} else {
+		return new Person(name, age, job);
+	}
+}; //继承需要用原型链或者寄生组合，避免this指定问题
+
+//惰性载入函数，避免if分支过多造成的运行缓慢，两种方法
+function createXHR() {
+	
+	if (typeof XMLHttpRequest != 'undefined') {
+		return new XMLHttpRequest();
+	} else if (typeof ActiveXObject != 'undefined') {
+		
+		if (typeof arguments.callee.activeXString != 'string') {
+			var versions = ['MSXML2.XMLHttp.6.0', 'MSXML2.XMLHttp.3.0', 'MSXML2.XMLHttp'],
+				i,
+				len;
+			for (i = 0, len = versions.length;i < len; i++) {
+				try{
+					new ActiveXObject(versions[i]);
+					arguments.callee.activeXString = versions[i];
+					break;
+				} catch(ex) {
+					//over
+				}
+			}
+		};
+		return new ActiveXObject(arguments.callee.activeXString);
+	} else {
+		throw Error('no xhr')
+	}
+};
+//第一种，函数被调用时处理函数
+function createXHR() {
+	
+	if (typeof XMLHttpRequest != 'undefined') {
+		createXHR = function()  { return new XMLHttpRequest();}
+	} else if (typeof ActiveXObject != 'undefined') {
+		createXHR = function()  {
+			if (typeof arguments.callee.activeXString != 'string') {
+				var versions = ['MSXML2.XMLHttp.6.0', 'MSXML2.XMLHttp.3.0', 'MSXML2.XMLHttp'],
+					i,
+					len;
+				for (i = 0, len = versions.length;i < len; i++) {
+					try{
+						new ActiveXObject(versions[i]);
+						arguments.callee.activeXString = versions[i];
+						break;
+					} catch(ex) {
+						//over
+					}
+				}
+			};
+			return new ActiveXObject(arguments.callee.activeXString);
+		};
+	} else {
+		createXHR = function()  {
+			throw Error('no xhr')
+		};
+	};
+	return createXHR();
+};
+//第二种，声明的时候指定适当的函数
+ var createXHR = (function() {
+	
+	if (typeof XMLHttpRequest != 'undefined') {
+		return function()  {
+			new XMLHttpRequest();
+		};
+	} else if (typeof ActiveXObject != 'undefined') {
+		return function() {
+			if (typeof arguments.callee.activeXString != 'string') {
+				var versions = ['MSXML2.XMLHttp.6.0', 'MSXML2.XMLHttp.3.0', 'MSXML2.XMLHttp'],
+					i,
+					len;
+				for (i = 0, len = versions.length;i < len; i++) {
+					try{
+						new ActiveXObject(versions[i]);
+						arguments.callee.activeXString = versions[i];
+						break;
+					} catch(ex) {
+						//over
+					}
+				}
+			};
+			return new ActiveXObject(arguments.callee.activeXString);
+		};
+	} else {
+		return function() {
+			throw Error('no xhr')
+		};
+	}
+})();
 //函数柯里化
 function curry(fn) {
 	var argus = Array.prototype.slice.call(arguments, 1);
