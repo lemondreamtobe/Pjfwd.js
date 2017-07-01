efd2bfd122058c8c5ed3da0ee3814b91cbb36821
Lemon_zhang <897418436@qq.com>
2017-07-01 22:56:35 +0800

    suibian

--- 04.chapter/js/4cp.js
+++ 04.chapter/js/4cp.js
@@ -7,7 +7,13 @@ function setName(obj) {
 var peron = new Object();
 setName(person); //person.name -'lin'
 
-//typeof & instanceo
+//引用类型按值传递实际上可以理解成在内部重写这个obj时引用的就是一个局部对象，函数运行后就会被销毁了
+//个人理解就是把引用的地址当做一个值来传递，当重新赋值会改写这个变量。
+
+//typeof & instanceof
+//前者主要检测基本类型
+//后者检测引用类型，不过在框架集中也存在问题
+//后者检测引用类型最好方法还是用toString(),Gm.js有相应的函数
 
 //标记清除 & 引用计数
 /*
